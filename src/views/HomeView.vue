11b5507c38b8005ae50a419c1ededa45e98fb515
galihtrisna <galihtrisna666@gmail.com>
2022-08-24 14:24:09 +0700

    project vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PatnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenShayna from '@/components/WomenShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PatnerShayna from '@/components/PatnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PatnerShayna,
+    FooterShayna
+}
+}
+</script>
